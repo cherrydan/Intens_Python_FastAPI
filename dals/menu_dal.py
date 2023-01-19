ad44b0be431818c4f9c4b648ea7a19373f785079
cherrydan <cherrydan.1978@gmail.com>
2023-01-19 21:29:14 +0300

    starts create routing to main menu

---
+++ dals/menu_dal.py
@@ -0,0 +1,25 @@
+#########################################################
+#               Main menu Data-Access-Layer             #
+#########################################################
+from typing import List
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from models.menu import Menu
+
+
+class MenuDAL:
+    def __init__(self, db_session: AsyncSession):
+        self.db_session = db_session
+
+    async def create_menu(self, title: str, description: str) -> Menu:
+        new_menu = Menu(title=title, description=description)
+        self.db_session.add(new_menu)
+        await self.db_session.flush()
+        return new_menu
+
+    async def get_all_menu(self) -> List[Menu]:
+        q = await self.db_session.execute(select(Menu).order_by(Menu.menu_id))
+        return q.scalars().all()
+
---
+++ routers/menu_router.py
@@ -0,0 +1,30 @@
+#########################################################
+#               ROUTING FOR Main MENU DAL               #
+#########################################################
+from typing import List
+
+from fastapi import APIRouter
+
+from dals.menu_dal import MenuDAL
+from db.config import async_session
+from models.menu import Menu
+
+router = APIRouter()
+
+
+# create main menu
+@router.post("/api/v1/menus")
+async def create_main_menu(title: str, description: str):
+    async with async_session() as session:
+        async with session.begin():
+            menu_dal = MenuDAL(session)
+            return await menu_dal.create_menu(title, description)
+
+
+# view main menu
+@router.get("/api/v1/menus")
+async def get_all_menus() -> List[Menu]:
+    async with async_session() as session:
+        async with session.begin():
+            menu_dal = MenuDAL(session)
+            return await menu_dal.get_all_menu()
