64913be891e4d7f4261414923dc00f2563f6d40a
cherrydan <cherrydan.1978@gmail.com>
2023-01-27 12:16:25 +0300

    Add title to application (for Swagger Docs)

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
-from typing import List
-
-from db.config import engine, Base
-from routers import menu_router
 import uvicorn
 from fastapi import FastAPI
 
-app = FastAPI()
+from db.config import engine, Base
+from routers import menu_router
+
+app = FastAPI(title="YLab University Python Intense Course")
 app.include_router(menu_router.router)
 
 
 @app.on_event("startup")
 async def startup():
     async with engine.begin() as conn:
-
         await conn.run_sync(Base.metadata.create_all)
 
 
